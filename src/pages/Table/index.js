56b4d9fd4e7defb58b97c86dd2e6a8346a30b6b8
Parizotto <parizottojs@gmail.com>
2023-03-06 19:57:45 -0300

    ATIVIDADE-002 adicionando pesquisa e checkbox com filtro para pelo menos 50 produtos disponíveis, partir para outros passos e talvez voltar customizar mais os dois

--- src/pages/Table/index.js
+++ src/pages/Table/index.js
@@ -1,28 +1,72 @@
 import style from "./Table.module.scss";
+import { useState } from "react";
 import tableData from "../../assets/repositorio.json";
 
 export default function Table() {
-  console.log(tableData);
-  const header = ["Produto", "Quantidade", " Peças Defeituosas", "Peças a Venda"/* , "Valor" */];
+  const [search, setSearch] = useState("");
+  const [checked, setChecked] = useState(false);
+  const header = [
+    "Produto",
+    "Quantidade",
+    " Peças Defeituosas",
+    "Peças a Venda" /* , "Valor" */,
+  ];
+  const updateCheckbox = () => {
+    setChecked(!checked);
+  };
+  
   return (
     <div className={style.tablePage}>
+      <div>
+        <input
+          className={style.input}
+          placeholder="Procure um produto"
+          onChange={(event) => setSearch(event.target.value)}
+        />
+      </div>
+      <div className={style.checkbox}>
+        <input
+          type="checkbox"
+          id="checkbox"
+          checked={checked}
+          onChange={updateCheckbox}
+        />
+        <label htmlFor="checkbox">Exibir produtos com 50 peças ou mais</label>
+      </div>
       <table className={style.table}>
-        <tr className={style.tableHeader}>
-          {header.map((name) => {
-            return <th>{name}</th>;
-          })}
-        </tr>
-        {tableData.map((item) => {
-          return (
-            <tr className={style.tableInfo}>
-              <td>{item.produto}</td>
-              <td>{item.quantidade}</td>
-              <td>{item.defeito}</td>
-              <td>{item.quantidade - item.defeito}</td>
-              {/* <td>{item.valor} RS</td> */}
-            </tr>
-          );
-        })}
+        <thead>
+          <tr className={style.tableHeader}>
+            {header.map((name, ind) => {
+              return <th key={ind}>{name}</th>;
+            })}
+          </tr>
+        </thead>
+        <tbody>
+          {tableData
+            .filter((data) => {
+              if(checked === true && data.quantidade - data.defeito < 50){
+                return false
+              }
+              if (search === "") {
+                return data;
+              } else if (
+                data.produto.toLowerCase().includes(search.toLowerCase())
+              ) {
+                return data;
+              }
+            })
+            .map((item, ind) => {
+              return (
+                <tr className={style.tableInfo} key={ind}>
+                  <td>{item.produto}</td>
+                  <td>{item.quantidade}</td>
+                  <td>{item.defeito}</td>
+                  <td>{item.quantidade - item.defeito}</td>
+                  {/* <td>{item.valor} RS</td> */}
+                </tr>
+              );
+            })}
+        </tbody>
       </table>
     </div>
   );
